2bcce14ba6050aecfc7a37c21999dd8a6e13e9b6
fe-dox <fedox@fedox.pl>
2024-09-29 01:58:05 +0200

    Enhance purchase process and modify cart form
    Wrapped `UserPurchase.create!` in a transaction block to handle multiple cart items and ensure data integrity. Modified the cart items view to use a form that posts to the purchases path, improving the user flow. Additionally, corrected a minor discrepancy in the product show view.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,7 +2,12 @@ class PurchasesController < ApplicationController
   def create
     user = User.first
 
-    UserPurchase.create!(user_id: user.id, product_id: params[:product][:id])
+    UserPurchase.transaction do
+      user.cart_items.each do |item|
+        UserPurchase.create!(user_id: user.id, product_id: item.product_id)
+        item.destroy!
+      end
+    end
 
     redirect_to root_path, notice: "Subscribed! Your order will be shipped as soon as possible."
   end
